5c353b99f0017f8ab3be9460bb8fe8a92f5bb76b
Adam Jonas <adamjonas25@gmail.com>
2024-04-26 22:49:54 +0100

    Add postorder test

--- src/test/kotlin/io/github/aj8gh/kalgo/algorithm/binarytree/PostorderTraversalKtTest.kt
+++ src/test/kotlin/io/github/aj8gh/kalgo/algorithm/binarytree/PostorderTraversalKtTest.kt
@@ -6,29 +6,28 @@ import io.github.aj8gh.kalgo.testutils.ARGUMENTS
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import kotlin.test.assertEquals
 
-class PreorderTraversalKtTest {
+class PostorderTraversalKtTest {
 
   @ParameterizedTest
   @MethodSource(ARGUMENTS)
-  fun <T> preorderTraversal(root: BinaryTreeNode<T>, expected: List<T>) =
-    assertEquals(expected, preorderTraversal(root))
+  fun <T> postorderTraversal(root: BinaryTreeNode<T>, expected: List<T>) =
+    kotlin.test.assertEquals(expected, postorderTraversal(root))
 
   companion object {
     @JvmStatic
     private fun arguments() = listOf(
       Arguments.of(
         binaryTreeFrom(listOf(1, 2, 3)),
-        listOf(1, 2, 3)
+        listOf(3, 2, 1)
       ),
       Arguments.of(
         binaryTreeFrom(listOf(1, 2, 3, 4, 5, 6, 7)),
-        listOf(1, 2, 4, 5, 3, 6, 7)
+        listOf(4, 5, 2, 6, 7, 3, 1)
       ),
       Arguments.of(
         binaryTreeFrom(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)),
-        listOf(1, 2, 4, 8, 9, 5, 10, 11, 3, 6, 12, 13, 7, 14, 15)
+        listOf(8, 9, 10, 11, 4, 5, 2, 12, 13, 14, 15, 6, 7, 3, 1)
       ),
     )
   }
